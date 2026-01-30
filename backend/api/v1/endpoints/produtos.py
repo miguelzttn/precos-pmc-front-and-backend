e80b355b5b5ae810062fec0aa98fbf6d2c12135b
miguelzttn <miguelzttn@gmail.com>
2026-01-30 18:04:28 -0300

    fix: where condition in wrong position

--- backend/api/v1/endpoints/produtos.py
+++ backend/api/v1/endpoints/produtos.py
@@ -180,10 +180,10 @@ async def get_produto(
             ') *Atualizado:* ' ||
             strftime(p.dt_referencia, '%d/%m/%Y') AS descricao
         FROM f_precos_completa p
-        WHERE p.vl_preco_atacado > 0
         LEFT JOIN d_empresas e
             ON p.nm_rede = e.nm_rede
             AND p.nm_bairro = e.nm_bairro
+        WHERE p.vl_preco_atacado > 0
         QUALIFY 
             ROW_NUMBER() OVER (PARTITION BY p.nm_rede, p.nm_bairro ORDER BY p.vl_preco_atacado ASC) == 1
         ORDER BY p.vl_preco_atacado ASC
